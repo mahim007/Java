cc216d66434253da2c39297895654ebecf6805e1
mahim007 <ashrafulmahim@gmail.com>
2019-10-13 18:57:40 +0600

    new-web-app added

---
+++ new-web-app/out/artifacts/new_web_app_war_exploded/index.jsp
@@ -0,0 +1,20 @@
+<%--
+  Created by IntelliJ IDEA.
+  User: Mahim007
+  Date: 29-Sep-19
+  Time: 9:05 PM
+  To change this template use File | Settings | File Templates.
+--%>
+<%@ page contentType="text/html;charset=UTF-8" language="java" %>
+<html>
+  <head>
+    <title>$Title$</title>
+  </head>
+  <body>
+  <form action="/login" method="post">
+    Name:<input type="text" name="userName"/><br/>
+    Password:<input type="password" name="userPass"/><br/>
+    <input type="submit" value="login"/>
+  </form>
+  </body>
+</html>
---
+++ new-web-app/src/main/java/WelcomeServlet.java
@@ -0,0 +1,16 @@
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+public class WelcomeServlet extends HttpServlet {
+    public void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        response.setContentType("text/html");
+        PrintWriter out = response.getWriter();
+
+        String name = request.getParameter("userName");
+        out.print("Hello " + name);
+    }
+}
