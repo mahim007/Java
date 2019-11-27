1a2df8b691116602a2e4d322e3416d4a4bd36899
mahim007 <ashraful.mahim@bs-23.net>
2019-11-27 20:13:24 +0600

    reflection api example

---
+++ advance-java/src/main/java/Sample.java
@@ -0,0 +1,11 @@
+public class Sample {
+    private String name = "mahim";
+
+    public String getName1() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
---
+++ advance-java/src/main/java/Test.java
@@ -0,0 +1,7 @@
+public class Test {
+    public static void main(String[] args) throws ClassNotFoundException, IllegalAccessException, InstantiationException {
+        Sample sample = new Sample();
+        System.out.println(sample.getName1());
+
+    }
+}
