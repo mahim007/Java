c76bff12b66228b4f5e24b2d7ade13c762881f98
mahim-bs <ashraful.mahim@bs-23.net>
2019-12-10 16:15:07 +0600

    commit for advance-java and new-web-app

--- advance-java/src/main/java/Test.java
+++
@@ -1,7 +0,0 @@
-public class Test {
-    public static void main(String[] args) throws ClassNotFoundException, IllegalAccessException, InstantiationException {
-        Sample sample = new Sample();
-        System.out.println(sample.getName1());
-
-    }
-}
